f1ed7e5641d2a507fde666d8c6aa1ce8fa7efef6
Tasnuva Huda <tasnuvah88@gmail.com>
2017-12-14 03:34:42 -0500

    final

--- config/orm.js
+++ config/orm.js
@@ -10,21 +10,14 @@ function printQuestionMarks(num) {
     return arr.toString();
   }
   
-  // Helper function to convert object key/value pairs to SQL syntax
   function objToSql(ob) {
     var arr = [];
-  
-    // loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
+      for (var key in ob) {
       var value = ob[key];
-      // check to skip hidden properties
       if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
         if (typeof value === "string" && value.indexOf(" ") >= 0) {
           value = "'" + value + "'";
         }
-        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-        // e.g. {sleepy: true} => ["sleepy=true"]
         arr.push(key + "=" + value);
       }
     }
@@ -43,7 +36,6 @@ var orm = {
       },
 
 insertOne: function(table, cols, vals, cb) {
-		// Construct the query string that inserts a single row into the target table
 		var queryString = "INSERT INTO " + table;
 
 		queryString += " (";
@@ -53,15 +45,11 @@ insertOne: function(table, cols, vals, cb) {
 		queryString += printQuestionMarks(vals.length);
 		queryString += ") ";
 
-		// console.log(queryString);
-
-		// Perform the database query
 		connection.query(queryString, vals, function(err, result) {
 			if (err) {
 				throw err;
 			}
 
-			// Return results in callback
 			cb(result);
 		});
 	},
@@ -74,15 +62,11 @@ insertOne: function(table, cols, vals, cb) {
             queryString += " WHERE ";
             queryString += condition;
     
-            // console.log(queryString);
-    
-            // Perform the database query
             connection.query(queryString, function(err, result) {
                 if (err) {
                     throw err;
                 }
     
-                // Return results in callback
                 cb(result);
             });
         }
